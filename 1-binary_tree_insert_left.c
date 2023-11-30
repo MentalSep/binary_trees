5da96ffffc73e5c14fca202fe317b8b9fae0f57c
MentalSep <chahidjava@hotmail.com>
2023-11-30 18:09:14 +0100

    add task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left child of another node
+ * @parent: pointer to parent node
+ * @value: value to put in node
+ *
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (!parent)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+
+	node->left = parent->left;
+	node->right = NULL;
+	node->n = value;
+	node->parent = parent;
+	parent->left = node;
+	if (node->left)
+		node->left->parent = node;
+
+	return (node);
+}
